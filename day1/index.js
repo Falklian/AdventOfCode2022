19cb6779b5958d269611f835b43a16fbe88dd501
Bill Walker <falklian@gmail.com>
2022-12-01 12:57:10 -0600

    day 1

---
+++ day1/index.js
@@ -0,0 +1,18 @@
+import { readFileSync } from 'node:fs';
+
+const data = readFileSync('day1.txt', 'utf8');
+
+const elfCalories = data.trim().split('\n\n');
+
+const calorieRank = [];
+elfCalories.forEach(elf => {
+  const calories = elf.split('\n').map(Number);
+  //const calories = calorieStrings.map(num => Number.parseInt(num));
+  calorieRank.push(calories.reduce((sum, value) => sum + value));
+});
+
+calorieRank.sort((a, b) => b - a);
+
+console.log(`Most Calories: ${calorieRank[0]}`);
+
+console.log(`Top 3 Calories: ${calorieRank.slice(0, 3).reduce((sum, value) => sum + value)}`);
